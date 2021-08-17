e234703df552ae3636f63a099de7355ef6b91db1
Anton Baronov <anton.baronov@gmail.com>
2021-08-17 14:09:19 +0300

    Update default.user.js

--- default.user.js
+++ default.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name         Default
+// @namespace    http://tampermonkey.net/
 // @version      0.1
+// @run-at       document-start
 // @description  try to take over the world!
-// @run-at      document-start
 // @author       You
-// @match        *
-// @grant        GM_addStyle
+// @match        *://*/*
 // @require https://code.jquery.com/jquery-3.6.0.min.js
+// @grant        GM_addStyle
 // ==/UserScript==
-(function() {
-  GM_addStyle("img{display: none !important; visibility: hidden !important} *{background-image:none !important}");
 
-  var o = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function(){
-    var res = o.apply(this, arguments);
-    this.setRequestHeader('__mid', '<<MACHINEID>>');
-    return res;
-  }
-  
-  fetch('https://parser.shiva-property.dev.daturum.ru/__developer/public_viewer/start/next_step.json')
+(function() {
+  var __machineID = '<<MACHINEID>>';
+
+  GM_addStyle("svg,img{display: none !important; visibility: hidden !important} *{background-image:none !important}");
+
+  //var o = XMLHttpRequest.prototype.open;
+  //XMLHttpRequest.prototype.open = function(){
+  //  var res = o.apply(this, arguments);
+  //  this.setRequestHeader('__mid', '<<MACHINEID>>');
+  //  return res;
+  //}
+
+  fetch('https://parser.shiva-property.dev.daturum.ru/__developer/public_viewer/start/next_step.json?__mid='+__machineID)
     .then(response => response.json())
     .then(data => console.log(data));
-})
+})();
