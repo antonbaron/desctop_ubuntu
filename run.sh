f016baab67283f916c612ce7a55252cf8c10cd67
Anton Baronov <anton.baronov@gmail.com>
2021-08-18 11:06:16 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sed -i "s~<<EXTURL>>~$EXTURL~g" /.config/falkon/profiles/default/extensions/greasemonkey/default.user.js
-falkon $EXTURL
 set -ex
 exec supervisord -c /system/supervisord.conf
