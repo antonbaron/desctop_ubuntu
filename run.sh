4260365dfb12bf199e7f01c77cfddea79d70f7b4
Anton Baronov <anton.baronov@gmail.com>
2021-08-18 19:21:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ sed -i "s~<<EXTURL>>~$EXTURL~g" /.config/falkon/profiles/default/extensions/grea
 sed -i "s~<<INTURL>>~$INTURL~g" /.config/falkon/profiles/default/extensions/greasemonkey/default.user.js
 sed -i "s~<<EXTURL>>~$EXTURL~g" /.config/falkon/profiles/default/settings.ini
 sed -i "s/<<MACHINEID>>/`cat /etc/machine-id`/g" /.config/falkon/profiles/default/extensions/greasemonkey/default.user.js
+
 set -ex
 exec supervisord -c /system/supervisord.conf
